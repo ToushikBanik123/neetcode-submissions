217a3ec654a37d49bf0ac79d2eb2ffe1c56b49f0
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2026-05-25 13:24:56 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+   public:
+    // void solve(int i, int n, int k, vector<int>& curr, vector<vector<int>>& res) {
+    //     if (curr.size() == k) {
+    //         res.push_back(curr);
+    //         return;
+    //     }
+    //     if (i > n) return;
+
+    //     // Include i
+    //     curr.push_back(i);
+    //     solve(i + 1, n, k, curr, res);
+    //     curr.pop_back();
+
+    //     // Exclude i
+    //     solve(i + 1, n, k, curr, res);
+    // }
+    void solve(int start, int n, int k, vector<int>& curr, vector<vector<int>>& res) {
+    if (curr.size() == k) { res.push_back(curr); return; }
+
+    for (int i = start; i <= n; i++) {
+        if (n - i + 1 < k - (int)curr.size()) break;  // pruning
+        curr.push_back(i);
+        solve(i+1, n, k, curr, res);
+        curr.pop_back();
+    }
+}
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> curr;
+        solve(1,n,k,curr,res);
+        return res;
+    }
+};
