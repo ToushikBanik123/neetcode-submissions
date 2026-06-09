b53917ad647d1d0a5ace30df1426515924173f6b
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2026-06-09 14:53:42 +0530

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    int uniquePathsWithObstacles(vector<vector<int>>& mx) {
+        int n = mx.size();
+        int m = mx[0].size();
+        if (n == 0 || m == 0 || mx[0][0] == 1 | mx[n - 1][m - 1] == 1) return 0;
+
+        vector<long long> dp(m, 0);
+        dp[m - 1] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (i == n - 1 && j == m - 1) continue;
+                if (mx[i][j] == 1) {
+                    dp[j] = 0;
+                } else if (j + 1 < m) {
+                    dp[j] += dp[j + 1];
+                }
+            }
+        }
+        return dp[0];
+    }
+};
